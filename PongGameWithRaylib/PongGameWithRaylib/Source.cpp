9cf7321bad7c63160fd21a6d38446d6de4e7c910
Kaan Arslan <kaan.arslan@ug.bilkent.edu.tr>
2024-06-10 18:42:03 +0300

    created game loop and black screen windows.

--- PongGameWithRaylib/PongGameWithRaylib/Source.cpp
+++ PongGameWithRaylib/PongGameWithRaylib/Source.cpp
@@ -6,6 +6,24 @@ using namespace std;
 int main() {
 
     cout << "Starting the game" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
 
+    InitWindow(screen_width, screen_height, "My pong game");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false) {
+
+        BeginDrawing();
+        EndDrawing();
+
+
+    }
+
+
+
+
+
+    CloseWindow();
     return 0;
 }
